d5288db2acf591f0c35596ec195be59cf3e58fb5
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2026-01-26 13:03:15 +0100

    implement mock logging

--- Godot/Core.cs
+++ Godot/Core.cs
@@ -4,14 +4,14 @@ namespace Godot
 {
 	public static class GD
 	{
-		public static void Print(params object[] args) => throw new NotImplementedException("Godot.GD.Print");
-		public static void PrintRaw(params object[] args) => throw new NotImplementedException("Godot.GD.PrintRaw");
-		public static void PrintRich(params object[] args) => throw new NotImplementedException("Godot.GD.PrintRich");
-		public static void PrintS(params object[] args) => throw new NotImplementedException("Godot.GD.PrintS");
-		public static void PrintT(params object[] args) => throw new NotImplementedException("Godot.GD.PrintT");
-		public static void PrintErr(params object[] args) => throw new NotImplementedException("Godot.GD.PrintErr");
-		public static void PushError(params object[] args) => throw new NotImplementedException("Godot.GD.PushError");
-		public static void PushWarning(params object[] args) => throw new NotImplementedException("Godot.GD.PushWarning");
+		public static void Print(params object[] args) => Console.WriteLine(string.Join(" ", args));
+		public static void PrintRaw(params object[] args) => Console.Write(string.Join(" ", args));
+		public static void PrintRich(params object[] args) => Console.WriteLine(string.Join(" ", args));
+		public static void PrintS(params object[] args) => Console.WriteLine(string.Join(" ", args));
+		public static void PrintT(params object[] args) => Console.WriteLine(string.Join("\t", args));
+		public static void PrintErr(params object[] args) => Console.Error.WriteLine(string.Join(" ", args));
+		public static void PushError(params object[] args) => Console.Error.WriteLine($"[ERROR] {string.Join(" ", args)}");
+		public static void PushWarning(params object[] args) => Console.WriteLine($"[WARNING] {string.Join(" ", args)}");
 	}
 
 	public static class Engine
