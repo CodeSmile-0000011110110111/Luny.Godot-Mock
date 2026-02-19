255a66a90113004c4f281e24e458478e4417f6fb
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2026-02-19 18:46:51 +0100

    add Transform proxy and TransformMoveBlock

--- Godot/Nodes/Node3D.cs
+++ Godot/Nodes/Node3D.cs
@@ -1,14 +1,55 @@
-using System;
+﻿using System;
 
 namespace Godot
 {
-	public class Node3D : Node
+	public partial class Node3D : Node
 	{
 		public Vector3 Position { get; set; }
 		public Vector3 Rotation { get; set; }
+		public Quaternion Quaternion { get; set; } = Quaternion.Identity;
 		public Vector3 Scale { get; set; } = Vector3.One;
 		public Boolean Visible { get; set; } = true;
 
+		public Vector3 GlobalPosition
+		{
+			get
+			{
+				var parent = GetParent() as Node3D;
+				return parent == null ? Position : parent.GlobalPosition + Position;
+			}
+			set
+			{
+				var parent = GetParent() as Node3D;
+				Position = parent == null ? value : value - parent.GlobalPosition;
+			}
+		}
+
+		public Vector3 GlobalRotation
+		{
+			get
+			{
+				var parent = GetParent() as Node3D;
+				return parent == null ? Rotation : parent.GlobalRotation + Rotation;
+			}
+			set
+			{
+				var parent = GetParent() as Node3D;
+				Rotation = parent == null ? value : value - parent.GlobalRotation;
+			}
+		}
+
+		public Vector3 GlobalScale
+		{
+			get
+			{
+				var parent = GetParent() as Node3D;
+				return parent == null ? Scale : new Vector3(parent.GlobalScale.X * Scale.X, parent.GlobalScale.Y * Scale.Y, parent.GlobalScale.Z * Scale.Z);
+			}
+		}
+
 		public Boolean IsVisibleInTree() => Visible && (GetParent() is not Node3D p || p.IsVisibleInTree());
 	}
+
+	// stub to preserve 'partial' keyword
+	public partial class Node3D {}
 }
